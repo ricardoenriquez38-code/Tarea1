02daaae8a7962fb4a4739c4e60f1e908a0baa3ae
ricardoenriquez38-code <ricardoenriquez38@aragon.unam.mx>
2025-08-31 00:05:26 -0600

    Modificar tarea.py

--- tarea.py
+++ tarea.py
@@ -1,22 +1,21 @@
-def existe_par_doble_arreglo(arr, K):
-    n = len(arr)
+def encontrar_k(letras, K):
+    n = len(letras)
+    encontrado = False  
     for i in range(n):
-        for j in range(i + 1, n):
-            if arr[i] + arr[j] == K:
-                print(f"Par encontrado: {arr[i]} + {arr[j]} = {K}")
-                return True
-    return False
+        for j in range(i + 1, n): 
+            if letras[i] + letras[j] == K:
+                print(f"Par encontrado: {letras[i]} + {letras[j]} = {K}")
+                encontrado = True
+    return encontrado
 
-# Entrada de datos usando ciclo
-arr = []
-tamaño = int(input("¿Cuántos números quieres ingresar? "))
+letras = []
+numero = int(input("¿Cuántos números quieres ingresar? "))
 
-for i in range(tamaño):
-    num = int(input(f"Ingrese el número {i+1}: "))
-    arr.append(num)
-
+for i in range(numero):
+    letra = int(input(f"Ingrese el número {i+1}: "))
+    letras.append(letra)
+    
 K = int(input("Ingresa el valor de K: "))
+resultado = encontrar_k(letras, K)
 
-
-resultado = existe_par_doble_arreglo(arr, K)
-print("¿Existe el par?", resultado)
+print("¿Existe al menos un par que sume K?", resultado)
